61b713a8bb1ffc3263710687ab1fcafb12be26ce
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-03-20 07:14:31 +0000

    actualio parametros de conexion a mysql

--- database/data_connection.py
+++ database/data_connection.py
@@ -1,9 +1,14 @@
 
 # In Production:
-database_user = "organizamidia"
-database_passw = "Ih27k6!_f7Tw"
-database_host = "den1.mysql1.gear.host"
-database_name = "organizamidia"
+# database_user = "organizamidia"
+# database_passw = "Ih27k6!_f7Tw"
+# database_host = "den1.mysql1.gear.host"
+# database_name = "organizamidia"
+
+database_user = "joaquindeveloper"
+database_passw = "jparrilla"
+database_host = "joaquindeveloper.mysql.pythonanywhere-services.com"
+database_name = "joaquindeveloper$organizamidia"
 
 # In development:
 dev_database_user = "root"
